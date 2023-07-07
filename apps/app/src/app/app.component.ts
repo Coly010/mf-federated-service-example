60ab4a821a22aaafc8f92a207b4041db29dc3be0
Colum Ferry <cferry09@gmail.com>
2023-07-07 14:07:57 +0100

    Initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfexample-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'app';
+}
